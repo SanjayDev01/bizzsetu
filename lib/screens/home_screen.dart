e4cf7808b7fe01d2868428d8c3acc964552db296
Sanju Poojari <36479146+SanjayDev01@users.noreply.github.com>
2022-05-22 22:41:31 +0530

    pull to refresh added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:bizzsetu/model/getdata_model.dart';
 import 'package:bizzsetu/services/http_service.dart';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,22 +12,51 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   GetData? _getData;
+  List<Data>? _dataList;
+  int pageNumber = 0;
+  final RefreshController refreshController =
+      RefreshController(initialRefresh: true);
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getData();
+    getData(pageNumber);
   }
 
-  getData() async {
-    var response = await HttpService.hGet('https://reqres.in/api/users?page=');
+  Future<void> getData(int page) async {
+    var response =
+        await HttpService.hGet('https://reqres.in/api/users?page=$page');
 
     setState(() {
       _getData = GetData.fromJson(response.data);
     });
 
-    print(_getData!.perPage);
+    if (_dataList == null) {
+      setState(() {
+        _dataList = _getData!.data;
+      });
+    } else {
+      setState(() {
+        _dataList!.addAll(_getData!.data);
+      });
+    }
+
+    print(_dataList);
+  }
+
+  getMore() {
+    setState(() {
+      pageNumber = pageNumber + 2;
+    });
+    getData(pageNumber);
+  }
+
+  getDefault() {
+    setState(() {
+      pageNumber = 0;
+    });
+    getData(pageNumber);
   }
 
   @override
@@ -36,19 +66,37 @@ class _HomeScreenState extends State<HomeScreen> {
           title: const Text('GFG Pagination'),
           centerTitle: false,
           backgroundColor: Colors.green.shade600),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-              itemCount: 100,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text('Item $index'),
-                );
-              },
-            ),
-          ),
-        ],
+      body: SmartRefresher(
+        controller: refreshController,
+        enablePullUp: false,
+        onRefresh: () async {
+          bool result = await getDefault();
+          if (result) {
+            refreshController.refreshCompleted();
+          } else {
+            refreshController.refreshFailed();
+          }
+        },
+        onLoading: () async {
+          bool result = await getMore();
+          if (result) {
+            refreshController.loadComplete();
+          } else {
+            refreshController.loadFailed();
+          }
+        },
+        child: ListView.separated(
+          itemBuilder: (context, index) {
+            final data = _dataList![index];
+
+            return ListTile(
+              title: Text(data.firstName),
+              subtitle: Text(data.firstName),
+            );
+          },
+          separatorBuilder: (context, index) => const Divider(),
+          itemCount: _dataList!.length,
+        ),
       ),
     );
   }
