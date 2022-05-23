c34bd568c4af1236d006d921bd2114dae0aa9e68
Sanju Poojari <36479146+SanjayDev01@users.noreply.github.com>
2022-05-23 10:38:11 +0530

    UI done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:bizzsetu/model/getdata_model.dart';
 import 'package:bizzsetu/services/http_service.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
@@ -78,39 +79,101 @@ class _HomeScreenState extends State<HomeScreen> {
           centerTitle: false,
           backgroundColor: Colors.green.shade600),
       body: SmartRefresher(
-        controller: refreshController,
-        enablePullUp: true,
-        onRefresh: () async {
-          getDefault();
-          if (dataLoaded) {
-            refreshController.refreshCompleted();
-          } else {
-            refreshController.refreshFailed();
-          }
-        },
-        onLoading: () async {
-          getMore();
-          if (dataLoaded) {
-            refreshController.loadComplete();
-          } else {
-            refreshController.loadFailed();
-          }
-        },
-        child: dataLoaded
-            ? ListView.separated(
-                itemBuilder: (context, index) {
-                  final data = _dataList![index];
+          controller: refreshController,
+          enablePullUp: true,
+          footer: CustomFooter(
+            builder: (context, mode) {
+              Widget body;
+              if (mode == LoadStatus.idle) {
+                body = const Text("pull up load");
+              } else if (mode == LoadStatus.loading) {
+                body = const CupertinoActivityIndicator();
+              } else if (mode == LoadStatus.failed) {
+                body = const Text("Load Failed!Click retry!");
+              } else if (mode == LoadStatus.canLoading) {
+                body = const Text("release to load more");
+              } else if (mode == LoadStatus.noMore) {
+                body = const Text("Thats all the data!");
+              } else {
+                body = const Text("Thats all the data!");
+              }
+              return SizedBox(
+                height: 55.0,
+                child: Center(child: body),
+              );
+            },
+          ),
+          onRefresh: () async {
+            getDefault();
+            if (dataLoaded) {
+              refreshController.refreshCompleted();
+            } else {
+              refreshController.refreshFailed();
+            }
+          },
+          onLoading: () async {
+            getMore();
+            if (dataLoaded) {
+              refreshController.loadComplete();
+            } else {
+              refreshController.loadFailed();
+            }
+          },
+          child: dataLoaded
+              ? ListView.separated(
+                  itemBuilder: (context, index) {
+                    final data = _dataList![index];
 
-                  return ListTile(
-                    title: Text(data.firstName),
-                    subtitle: Text(data.firstName),
-                  );
-                },
-                separatorBuilder: (context, index) => const Divider(),
-                itemCount: _dataList!.length,
-              )
-            : const Center(child: CircularProgressIndicator()),
-      ),
+                    return Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      height: 170,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              left: 18.0,
+                              right: 18.0,
+                            ),
+                            child: Image.network(data.avatar,
+                                width: 140, height: 140, fit: BoxFit.cover),
+                          ),
+                          Expanded(
+                              child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Text(data.firstName,
+                                  style: const TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold)),
+                              Text(data.lastName,
+                                  style: const TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold)),
+                              Text(data.email,
+                                  style: const TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold)),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                            ],
+                          ))
+                        ],
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) => const Divider(),
+                  itemCount: _dataList!.length,
+                )
+              : const Center(
+                  child: CircularProgressIndicator(),
+                )),
     );
   }
 }
